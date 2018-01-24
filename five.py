f397fd406151dcc3ba862060c72b6691d7910253
Dhivya-Ra <35556359+Dhivya-Ra@users.noreply.github.com>
2018-01-24 16:17:36 +0530

    Create five.py

---
+++ five.py
@@ -0,0 +1,19 @@
+x = input(" ")
+y=  input(" ")
+z=  input(" ")
+print("inputs:",end=" ")
+print(x,end=" ")
+print(y,end=" ")
+print(z)
+print("o/p")
+if x>y :
+	if x>z :
+		print(x)
+	else:
+		print(z)
+elif y>z:
+	print(y)
+else:
+	print(z)
+
+
