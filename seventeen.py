30fa03f33e54fa7928a16e43aeafdda4a38b00ad
Dhivya-Ra <35556359+Dhivya-Ra@users.noreply.github.com>
2018-02-01 13:06:01 +0530

    Update seventeen.py

--- seventeen.py
+++ seventeen.py
@@ -1,14 +1,11 @@
 m=0
 x=int(input(""))
-if x<0:
-	print("enter positive integer")
+n=x
+while(x>0):
+	r=x%10
+	x=x//10
+	m=(r*r*r)+m
+if m==n:
+	print("yes")
 else:
-	n=x
-	while(x>0):
-		r=x%10
-		x=x//10
-		m=(r*r*r)+m
-	if m==n:
-		print("yes")
-	else:
-		print("no")
+	print("no")
