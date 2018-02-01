af1440f32cb08601f4ae2951b910f0d4206e7e54
Dhivya-Ra <35556359+Dhivya-Ra@users.noreply.github.com>
2018-02-01 12:33:30 +0530

    Create thirteen.py

---
+++ thirteen.py
@@ -0,0 +1,16 @@
+m=0
+x=int(input(""))
+if x<0:
+	print("enter positive integer")
+else:
+	if x%2==0:
+		print("no")
+	else:
+		for a in range(3,(x//2)+1):
+			if x%a == 0:
+				print("no")
+				m=m+1
+				break;
+		if m==0:
+			print("yes")
+		
