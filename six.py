75e588020c12e8f9e62202e97a43a9a2c4fb8dee
Dhivya-Ra <35556359+Dhivya-Ra@users.noreply.github.com>
2018-02-01 11:07:31 +0530

    Update six.py

--- six.py
+++ six.py
@@ -1,11 +1,14 @@
 x=int(input(""))
-if x%4 ==0 :
-	if x%100 == 0:
-		if x%400 ==0:
-			print('leap year')
-		else:
-			print('non leap year')
-	else:
-		print('leap year')
+if x<0:
+	print("enter valid input")
 else:
-	print('non leap year')
+	if x%4 ==0 :
+		if x%100 == 0:
+			if x%400 ==0:
+				print('leap year')
+			else:
+				print('non leap year')
+		else:
+			print('leap year')
+	else:
+		print('non leap year')
