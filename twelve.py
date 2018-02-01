eb54fbf146ad5fe0bb8ee02e314c2074df577f45
Dhivya-Ra <35556359+Dhivya-Ra@users.noreply.github.com>
2018-02-01 11:29:34 +0530

    Update twelve.py

--- twelve.py
+++ twelve.py
@@ -1,14 +1,17 @@
 
+
 x=int(input(""))
-n=x
-m=0
-while(x>0):
-	r=x%10
-	x=x//10
-	m=(m*10)+r
-	
-if m==n:
-	print("yes")
-else:
+if x<0:
 	print("no")
+else:
+	n=x
+	m=0
+	while(x>0):
+		r=x%10
+		x=x//10
+		m=(m*10)+r
+	if m==n:
+		print("yes")
+	else:
+                  	print("no")
 
