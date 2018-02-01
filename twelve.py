53574bc792a75f101447d6402c7cb8beb3e080a2
Dhivya-Ra <35556359+Dhivya-Ra@users.noreply.github.com>
2018-02-01 11:21:32 +0530

    Update twelve.py

--- twelve.py
+++ twelve.py
@@ -1,13 +1,14 @@
-x = int(input(""))
+
+x=int(input(""))
 n=x
 m=0
-while (x>0):
-	r=int(x)%10
-	print(r)
-            x=x/10
+while(x>0):
+	r=x%10
+	x=x//10
 	m=(m*10)+r
-	print(m)
-if m==n :
-	print("palindrome")
-else :
-	print("not palindrome")
+	
+if m==n:
+	print("yes")
+else:
+	print("no")
+
