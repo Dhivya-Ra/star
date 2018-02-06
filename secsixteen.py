415e688dfe4a1553b6bef1b8d535c91eefbf3c85
Dhivya-Ra <35556359+Dhivya-Ra@users.noreply.github.com>
2018-02-06 04:08:11 -0800

    Update secsixteen.py

--- secsixteen.py
+++ secsixteen.py
@@ -3,12 +3,20 @@ l=[]
 for a in range(0,n):
 	b=int(input(""))
 	l.append(b)
-for a in range(0,n,1):
-	m=0
+for a in range(0,n-1,1):
 	for b in range(a+1,n,1):
-		if l[a]==l[b]:
-			m=m+1
-	if m==0:
-		print(l[a])
-		break
-	
+		if l[a]<l[b]:
+			c=l[a]
+			l[a]=l[b]
+			l[b]=c
+for a in range(0,n,1):
+	if a>0 and a<n-1:
+		if l[a]!=l[a+1] and l[a]!=l[a-1]:
+			print(l[a])
+	elif a==0:
+		if l[a]!=l[a+1]:
+				print(l[a])
+	elif a==n-1:
+		if l[a]!=l[a-1]:
+				print(l[a])
+		
