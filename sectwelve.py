0bff42911e2313dd05c59d4b9bc69049a51c263c
Dhivya-Ra <35556359+Dhivya-Ra@users.noreply.github.com>
2018-02-06 02:04:59 -0800

    Update sectwelve.py

--- sectwelve.py
+++ sectwelve.py
@@ -1,19 +1,21 @@
 n= int(input(""))
 l=int(input(""))
 if l<0 or n<0:
-s=[]
-for a in range(n):
-	b=int(input(""))
-	s.append(b)
-b=0
-while(b<l):
-	d=s[0]
-	for a in range(1,n):
-		c=s[a]
-		s[a]=d
-		d=c
-	s[0]=d
-	b=b+1
-for a in s:
-	print(a,end=" ")
+	print("enter positive integer")
+else:
+	s=[]
+	for a in range(n):
+		b=int(input(""))
+		s.append(b)
+		b=0
+	while(b<l):
+		d=s[0]
+		for a in range(1,n):
+			c=s[a]
+			s[a]=d
+			d=c
+		s[0]=d
+		b=b+1
+	for a in s:
+		print(a,end=" ")
 	
