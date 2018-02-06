77a94c3ef5eb0ab1960f65ac2077ade7c2384a74
Dhivya-Ra <35556359+Dhivya-Ra@users.noreply.github.com>
2018-02-06 00:50:05 -0800

    Create seceleven.py

---
+++ seceleven.py
@@ -0,0 +1,9 @@
+s=input("")
+s=s.lower()
+#a="sunday"
+if s=="sunday" or s=="saturday":
+	print("yes")
+elif s=="monday" or s=="tuesday" or s=="wednesday" or s=="thursday" or s=="friday":
+	print("no")
+else:
+	print("enter name of the day")
